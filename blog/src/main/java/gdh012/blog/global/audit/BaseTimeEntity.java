be76e0970c6060b81b9c790e07dd11c95d971f64
dohyoungK <gdh012@pusan.ac.kr>
2024-10-14 18:42:57 +0900

    ✨ Feat : 생성일, 수정일 Auditing 구현

---
+++ blog/src/main/java/gdh012/blog/global/audit/BaseTimeEntity.java
@@ -0,0 +1,24 @@
+package gdh012.blog.global.audit;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.EntityListeners;
+import jakarta.persistence.MappedSuperclass;
+import lombok.Getter;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import java.time.LocalDateTime;
+
+@Getter
+@MappedSuperclass
+@EntityListeners(AuditingEntityListener.class)
+public abstract class BaseTimeEntity {
+    @CreatedDate
+    @Column(name = "CREATED_AT", updatable = false)
+    private LocalDateTime createdAt;
+
+    @LastModifiedDate
+    @Column(name = "LAST_MODIFIED_AT")
+    private LocalDateTime modifiedAt;
+}
