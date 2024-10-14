fa76f81c458aa25aa04c518d8c67ff99f8f1562b
dohyoungK <gdh012@pusan.ac.kr>
2024-10-14 18:42:01 +0900

    🔧 Conf : 프로젝트 초기 설정

---
+++ blog/src/test/java/gdh012/blog/BlogApplicationTests.java
@@ -0,0 +1,13 @@
+package gdh012.blog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BlogApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
