88f8b09129c901717e7b47731c2d722d21dca6ac
dohyoungK <gdh012@pusan.ac.kr>
2024-10-21 17:35:14 +0900

    ✨ Feat : Jwt Filter, Service 구현

--- blog/src/main/java/gdh012/blog/global/jwt/service/JwtService.java
+++ blog/src/main/java/gdh012/blog/global/jwt/service/JwtService.java
@@ -67,8 +67,8 @@ public class JwtService {
     public void sendAccessAndRefreshToken(HttpServletResponse response, String accessToken, String refreshToken) {
         response.setStatus(HttpServletResponse.SC_OK);
 
-        setAccessTokenHeader(response, accessToken);
-        setRefreshTokenHeader(response, refreshToken);
+        setAccessTokenHeader(response, BEARER + accessToken);
+        setRefreshTokenHeader(response, BEARER + refreshToken);
         log.info("Access Token, Refresh Token 헤더 설정 완료");
     }
 
