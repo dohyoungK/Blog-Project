4194abaab2d93d0c5d34865039039bf0fdbd7596
dohyoungK <gdh012@pusan.ac.kr>
2024-10-21 17:36:12 +0900

    ✨ Feat : OAuth2 소셜 로그인 구현

--- blog/src/main/java/gdh012/blog/global/oauth2/CustomOAuth2User.java
+++ blog/src/main/java/gdh012/blog/global/oauth2/CustomOAuth2User.java
@@ -13,7 +13,9 @@ public class CustomOAuth2User extends DefaultOAuth2User {
     private String email;
     private Role role;
 
-    public CustomOAuth2User(Collection<? extends GrantedAuthority> authorities, Map<String, Object> attributes, String nameAttributeKey, String email, Role role) {
+    public CustomOAuth2User(Collection<? extends GrantedAuthority> authorities,
+                            Map<String, Object> attributes, String nameAttributeKey,
+                            String email, Role role) {
         super(authorities, attributes, nameAttributeKey);
         this.email = email;
         this.role = role;
--- blog/src/main/java/gdh012/blog/global/oauth2/OAuthAttributes.java
+++ blog/src/main/java/gdh012/blog/global/oauth2/OAuthAttributes.java
@@ -5,18 +5,17 @@ import gdh012.blog.domain.account.constants.SocialType;
 import gdh012.blog.domain.account.entity.Account;
 import gdh012.blog.global.oauth2.userInfo.GoogleOAuth2UserInfo;
 import gdh012.blog.global.oauth2.userInfo.KakaoOAuth2UserInfo;
+import gdh012.blog.global.oauth2.userInfo.NaverOAuth2UserInfo;
 import gdh012.blog.global.oauth2.userInfo.OAuth2UserInfo;
 import lombok.Builder;
 import lombok.Getter;
-import org.springframework.security.core.userdetails.User;
 
 import java.util.Map;
-import java.util.UUID;
 
 @Getter
 public class OAuthAttributes {
-    private String nameAttributeKey; // OAuth2 로그인 진행 시 키가 되는 필드 값, PK와 같은 의미
-    private OAuth2UserInfo oauth2UserInfo; // 소셜 타입별 로그인 유저 정보(닉네임, 이메일, 프로필 사진 등등)
+    private String nameAttributeKey;
+    private OAuth2UserInfo oauth2UserInfo;
 
     @Builder
     private OAuthAttributes(String nameAttributeKey, OAuth2UserInfo oauth2UserInfo) {
@@ -24,17 +23,13 @@ public class OAuthAttributes {
         this.oauth2UserInfo = oauth2UserInfo;
     }
 
-    /**
-     * SocialType에 맞는 메소드 호출하여 OAuthAttributes 객체 반환
-     * 파라미터 : userNameAttributeName -> OAuth2 로그인 시 키(PK)가 되는 값 / attributes : OAuth 서비스의 유저 정보들
-     * 소셜별 of 메소드(ofGoogle, ofKaKao, ofNaver)들은 각각 소셜 로그인 API에서 제공하는
-     * 회원의 식별값(id), attributes, nameAttributeKey를 저장 후 build
-     */
     public static OAuthAttributes of(SocialType socialType,
                                      String userNameAttributeName, Map<String, Object> attributes) {
-
         if (socialType == SocialType.KAKAO) {
             return ofKakao(userNameAttributeName, attributes);
+        }
+        if (socialType == SocialType.NAVER) {
+            return ofNaver(userNameAttributeName, attributes);
         }
         return ofGoogle(userNameAttributeName, attributes);
     }
@@ -46,6 +41,13 @@ public class OAuthAttributes {
                 .build();
     }
 
+    private static OAuthAttributes ofNaver(String userNameAttributeName, Map<String, Object> attributes) {
+        return OAuthAttributes.builder()
+                .nameAttributeKey(userNameAttributeName)
+                .oauth2UserInfo(new NaverOAuth2UserInfo(attributes))
+                .build();
+    }
+
     public static OAuthAttributes ofGoogle(String userNameAttributeName, Map<String, Object> attributes) {
         return OAuthAttributes.builder()
                 .nameAttributeKey(userNameAttributeName)
@@ -53,17 +55,12 @@ public class OAuthAttributes {
                 .build();
     }
 
-    /**
-     * of메소드로 OAuthAttributes 객체가 생성되어, 유저 정보들이 담긴 OAuth2UserInfo가 소셜 타입별로 주입된 상태
-     * OAuth2UserInfo에서 socialId(식별값), nickname, imageUrl을 가져와서 build
-     * email에는 UUID로 중복 없는 랜덤 값 생성
-     * role은 GUEST로 설정
-     */
     public Account toEntity(SocialType socialType, OAuth2UserInfo oauth2UserInfo) {
         return Account.builder()
                 .socialType(socialType)
-                .email(oauth2UserInfo.getId() + "@" + socialType.name() + ".com")
+                .email(oauth2UserInfo.getId() + "@" + socialType.name() + ".com") // 소셜 계정의 식별자를 활용해 email 생성
                 .nickname(oauth2UserInfo.getNickname())
+                .password(socialType.name() + oauth2UserInfo.getId())
                 .profileImageUrl(oauth2UserInfo.getProfileImageUrl())
                 .role(Role.GUEST)
                 .build();
