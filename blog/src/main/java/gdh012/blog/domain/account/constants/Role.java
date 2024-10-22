d49812ef3c98f0c978c3189e354985d5b853785b
dohyoungK <gdh012@pusan.ac.kr>
2024-10-22 21:32:13 +0900

    ✨ Feat : role admin 추가

--- blog/src/main/java/gdh012/blog/domain/account/constants/Role.java
+++ blog/src/main/java/gdh012/blog/domain/account/constants/Role.java
@@ -7,7 +7,8 @@ import lombok.RequiredArgsConstructor;
 @RequiredArgsConstructor
 public enum Role {
     GUEST("ROLE_GUEST"),
-    USER("ROLE_USER");
+    USER("ROLE_USER"),
+    ADMIN("ROLE_ADMIN");
 
     private final String key;
 }
