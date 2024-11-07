05f9dbffb8ea38d8b4174038f3438a2a8880d6a9
dohyoungK <gdh012@pusan.ac.kr>
2024-11-07 17:19:59 +0900

    🔧 Conf : restdocs test용 config 설정

---
+++ blog/src/test/java/gdh012/blog/global/util/TestSetUpUtil.java
@@ -0,0 +1,42 @@
+package gdh012.blog.global.util;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import gdh012.blog.global.config.RestDocsConfiguration;
+import gdh012.blog.global.config.TestSecurityConfiguration;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Disabled;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Import;
+import org.springframework.restdocs.RestDocumentationContextProvider;
+import org.springframework.restdocs.RestDocumentationExtension;
+import org.springframework.restdocs.mockmvc.RestDocumentationResultHandler;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.web.context.WebApplicationContext;
+
+import static org.springframework.restdocs.mockmvc.MockMvcRestDocumentation.documentationConfiguration;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.setup.MockMvcBuilders.webAppContextSetup;
+
+@Disabled
+@Import({TestSecurityConfiguration.class, RestDocsConfiguration.class})
+@ExtendWith(RestDocumentationExtension.class)
+public class TestSetUpUtil {
+    @Autowired
+    protected RestDocumentationResultHandler restDocs;
+
+    @Autowired
+    protected MockMvc mockMvc;
+
+    @Autowired
+    protected ObjectMapper objectMapper;
+
+    @BeforeEach
+    public void setUp(final WebApplicationContext context, final RestDocumentationContextProvider provider) {
+        this.mockMvc = webAppContextSetup(context)
+                .apply(documentationConfiguration(provider))
+                .alwaysDo(print())
+                .alwaysDo(restDocs)
+                .build();
+    }
+}
