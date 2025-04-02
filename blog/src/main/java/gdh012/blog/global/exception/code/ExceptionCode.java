53fd7012834e99f966b5d244b0a3e2678e0824b3
dohyoungK <gdh012@pusan.ac.kr>
2025-04-02 18:34:46 +0900

    ✨ Feat : Tag 관련 예외코드 추가

--- blog/src/main/java/gdh012/blog/global/exception/code/ExceptionCode.java
+++ blog/src/main/java/gdh012/blog/global/exception/code/ExceptionCode.java
@@ -18,7 +18,7 @@ public enum ExceptionCode {
     ACCOUNT_ACCESS_DENIED(403, "AUTH-006", "Access is Denied"),
 
     ACCOUNT_NOT_FOUND(404, "ACCOUNT-001", "Account Not Found"),
-    ACCOUNT_NOT_ALLOW(405, "ACCOUNT-002", "That Account doesn't have authority"),
+    ACCOUNT_NOT_ALLOW(401, "ACCOUNT-002", "That Account doesn't have authority"),
     ACCOUNT_ALREADY_EXISTS(409, "ACCOUNT-003", "Account Already Exists"),
     EMAIL_ALREADY_EXISTS(409, "ACCOUNT-004", "Email Already Exists"),
     NICKNAME_ALREADY_EXISTS(409, "ACCOUNT-005", "Nickname Already Exists"),
@@ -26,16 +26,24 @@ public enum ExceptionCode {
 
 
     BOARD_NOT_FOUND(404, "BOARD-001", "Board Not Found"),
-    BOARD_NOT_ALLOW(405, "BOARD-002", "That Board doesn't have authority"),
+    BOARD_NOT_ALLOW(401, "BOARD-002", "That Board doesn't have authority"),
     BOARD_ALREADY_EXISTS(409, "BOARD-003", "Board Already Exists"),
 
     BOARD_IMAGE_NOT_FOUND(404, "BOARD-004", "Board Image Not Found"),
-    BOARD_IMAGE_NOT_ALLOW(405, "BOARD-005", "That Board image doesn't have authority"),
+    BOARD_IMAGE_NOT_ALLOW(401, "BOARD-005", "That Board image doesn't have authority"),
     BOARD_IMAGE_ALREADY_EXISTS(409, "BOARD-006", "Board Image Already Exists"),
 
+    CATEGORY_NOT_FOUND(404, "CATEGORY-001", "Category Not Found"),
+    CATEGORY_NOT_ALLOW(401, "CATEGORY-002", "That Category doesn't have authority"),
+    CATEGORY_ALREADY_EXISTS(409, "CATEGORY-003", "Category Already Exists"),
+
     COMMENT_NOT_FOUND(404, "COMMENT-001", "Comment Not Found"),
-    COMMENT_NOT_ALLOW(405, "COMMENT-002", "That Comment doesn't have authority"),
-    COMMENT_ALREADY_EXISTS(409, "COMMENT-003", "Comment Already Exists");
+    COMMENT_NOT_ALLOW(401, "COMMENT-002", "That Comment doesn't have authority"),
+    COMMENT_ALREADY_EXISTS(409, "COMMENT-003", "Comment Already Exists"),
+
+    TAG_NOT_FOUND(404, "TAG-001", "Tag Not Found"),
+    TAG_NOT_ALLOW(401, "TAG-002", "That Tag doesn't have authority"),
+    TAG_ALREADY_EXISTS(409, "TAG-003", "Tag Already Exists");
 
     private final int status;
     private final String code;
